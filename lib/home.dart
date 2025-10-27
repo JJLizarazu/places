0e98a8ccd7dabf11e3980a21189a1a13f2643722
JJLizarazu05 <juanjoselizarazu2005@gmail.com>
2025-10-27 09:32:15 -0400

    Widget BorderRadius y BoxShadow - Widget ListView Horizontal - InkWell

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:places/card_image.dart';
 import 'package:places/gradient_back.dart';
+import 'package:places/home_app_bar.dart';
 import 'package:places/review.dart';
 import 'package:places/review_list.dart';
 
@@ -10,11 +12,11 @@ class MyHome extends StatelessWidget{
   Widget build(BuildContext context) {
     final descriptionPlace = Container(
       margin: EdgeInsets.only(
-        top: 250,
+        top: 300,
         left: 30,
         right: 30
       ),
-      child: DescriptionPlace("INFOCAL", 5, "VIVA EL RAFA Y SUS AMIGUITOS ! :D -> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+      child: DescriptionPlace("Uyuni", 5, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
     );
 
     final reviewList = Container(
@@ -36,7 +38,8 @@ class MyHome extends StatelessWidget{
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          GradientBack()
+          listView,
+          HomeAppBar("Popular")
         ],
       ),
     );
--- lib/home_app_bar.dart
+++ lib/home_app_bar.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:places/card_image.dart';
 import 'package:places/gradient_back.dart';
 
+import 'card_image_list.dart';
+
 class HomeAppBar extends StatelessWidget{
+
+  String textoTitulo;
+
+  HomeAppBar(this.textoTitulo);
+
   @override
   Widget build(BuildContext context) {
 
+
+    final titulo = Container(
+      margin: EdgeInsets.only(
+        top: 40,
+        left: 30
+      ),
+      child: Text(
+        textoTitulo,
+        style: TextStyle(
+          fontFamily: "Lato",
+          fontWeight: FontWeight.w900,
+          fontSize: 30,
+          color: Colors.white
+        ),
+      ),
+    );
+
     final appBar = Stack(
       children: <Widget>[
-        GradientBack()
+        GradientBack(),
+        titulo,
+        CardImageList()
       ],
     );
 
